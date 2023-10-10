fad53c6e87f294aa7e8507b970ca740eca61a7f6
zohra-neda <zohra.neda@gmail.com>
2023-10-10 13:43:57 +0430

    Create Food model and update foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,34 @@
 class FoodsController < ApplicationController
-  def index; end
+
+  def index
+    @foods = Food.all
+  end
 
   def show; end
+
+  def new
+    @food = Food.new
+  end
+
+  def Create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to @food, notice: 'Food item was successfully created.'
+    else
+      render :new
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to food_index_url, notice: 'Food item was successfully deleted.'
+  end
+
+  private
+
+  # Define permitted parameters for creating a new food item
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+end
 end
