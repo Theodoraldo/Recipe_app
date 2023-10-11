eadaa4f1f780580ec084a419970d6723fa4910e9
Theodoraldo <theodoraldo@gmail.com>
2023-10-11 04:22:06 +0000

    Create controller actions and view to add recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,25 @@ class RecipesController < ApplicationController
   def index; end
 
   def show; end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path, notice: 'New Recipe saved !!!.'
+    else
+      flash[:alert] = 'Failed to create recipe. Please check the form for errors.'
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
+  end
 end
