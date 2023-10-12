ca72b4d4c023f4ad292ea1ca65e21ba84a787c48
Theodoraldo <theodoraldo@gmail.com>
2023-10-12 00:57:29 +0000

    Functionality to add ingredient and delete them

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,36 @@
 class RecipeFoodsController < ApplicationController
-  def index; end
+   before_action :set_recipe_food, only: [:destroy]
 
-  def show; end
+   def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.all
+    @recipe_food = @recipe.recipe_foods.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
+
+    if @recipe.user == current_user && @recipe_food.save
+      redirect_to @recipe, notice: 'Ingredient successfully added'
+    else
+      @foods = Food.all
+      render :new, status: :unprocessable_entity, alert: 'Error occured whiles adding ingredient (Food) !!!'
+    end
+  end
+
+  def destroy
+    @recipe_food.destroy
+    redirect_to recipe_url, notice: 'Recipe food was deleted !!!'
+  end
+
+  private
+
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+  end
 end
