611d8e5e2a9faaed4a90138bb46f5ee4ed358cb8
Theodoraldo <theodoraldo@gmail.com>
2023-10-12 17:34:23 +0000

    Add model method to toggle button

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+
+  def change_public_status
+    update(public: !public)
+  end
 end
