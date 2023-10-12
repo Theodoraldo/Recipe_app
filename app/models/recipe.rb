5e2ed1d3dc80e966a746aefe5a4600b1919fc5ce
Theodoraldo <theodoraldo@gmail.com>
2023-10-12 21:00:10 +0000

    Modify food and recioe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
 
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   def change_public_status
